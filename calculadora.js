a3ca4f5b8166d027dc83c017a567d9f1580abaab
Erian Santoy <leonardo88erian@gmail.com>
2026-01-22 13:37:33 -0600

    cambios

--- calculadora.js
+++ calculadora.js
@@ -1,14 +1,32 @@
 // alert("Hola mundo hermoso y maravilloso");
 
-function mostrar() {
+function mostrar(e) {
     let numero1 = parseInt(document.getElementById("numero1").value);
     let numero2 = parseInt(document.getElementById("numero2").value);
-
     let resultado = 0;
-    resultado = numero1 + numero2;
+    let operacion = e.target.id;
 
-    let objetoResultado = document.getElementById("resultado");
-    objetoResultado.innerHTML = resultado;
+    if (operacion == "suma") {
+        resultado = numero1 + numero2;
+    }
+    else if (operacion == "resta") {
+        resultado = numero1 - numero2;
+    }
+    else if (operacion == "multiplica") {
+        resultado = numero1 * numero2;
+    }
+    else if (operacion == "divide") {
+        resultado = numero1 / numero2;
+    }
+
+    else ( alert("boton en proceso") );
+
+
+    let result = document.getElementById("resultado");
+    result.innerHTML = resultado;
 
    // alert(resultado);    
 }
+
+let butones = document.getElementById("btns");
+butones.addEventListener("click", mostrar);
